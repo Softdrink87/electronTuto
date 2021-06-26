f79af006cebb1d350afb34888376d1048da6b5f9
Softdrink87 <36359312+Softdrink87@users.noreply.github.com>
2021-06-26 18:04:15 +0900

    Add files via upload

---
+++ clock/clock.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron')
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width:190,
+        height:80,
+        resizable:false
+    })
+    win.loadFile('clock.html')
+}
